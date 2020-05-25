97174e650066b9590666b56d3fa39c8fb632839b
Siequnu <11821606+Siequnu@users.noreply.github.com>
2020-05-25 17:50:57 +0800

    Add statement templates

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-bp = Blueprint('statements', __name__)
+bp = Blueprint('statements', __name__, template_folder = 'templates')
 
 from app.statements import routes, models, forms
